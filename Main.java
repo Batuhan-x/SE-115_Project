584457f1adb47b8fe1c25fa08dfaaba387148aa0
Batuhan-x <batukrs09@gmail.com>
2025-12-20 16:37:25 +0300

    added methods

--- Main.java
+++ Main.java
@@ -254,7 +254,38 @@ public class Main {
     }
 
     public static String compareTwoCommodities(String c1, String c2) {
-        return "DUMMY is better by 1234";
+        int c1check =-1;
+        int c2check=-1;
+        for(int i=0;i<commodities.length;i++) {
+            if (c1.equals(commodities[i])) {
+                c1check = i;
+
+            }if(c2.equals(commodities[i])){
+                c2check=i;
+            }
+        }
+        if(c2check==-1 || c1check==-1){
+            return "INVALID_COMMODITY";
+
+        }
+        int c1sum=0;
+        int c2sum=0;
+        int diff=0;
+        for(int i=0;i<months.length;i++) {
+            for (int j = 0; j < 28; j++) {
+                c1sum+=profits[i][j][c1check];
+                c2sum+=profits[i][j][c2check];
+            }
+        }
+        diff=Math.abs(c1sum-c2sum);
+        if(c1sum>c2sum){
+            return c1+" is better by "+diff;
+        }else if(c1sum<c2sum){
+            return c2+" is better by "+diff;
+        }else{
+            return "Equal";
+        }
+
     }
 
     public static String bestWeekOfMonth(int month) {
